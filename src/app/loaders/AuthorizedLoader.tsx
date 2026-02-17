4b6c5c8f4a7badbffa488413b83603af5600f4b2
alibeksopu <a.sopuev@itez.com>
2026-02-17 16:12:28 +0700

    every

--- src/app/loaders/AuthorizedLoader.tsx
+++ src/app/loaders/AuthorizedLoader.tsx
@@ -1,14 +1,31 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect, useRef } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
-import { useAppSelector } from '@shared/lib'
-import { selectIsAuthenticated } from '@entities/session'
+import { useAppSelector, useAppDispatch, isAllowedUser } from '@shared/lib'
+import { selectIsAuthenticated, selectUser, signOutThunk } from '@entities/session'
 
 export const AuthorizedLoader = ({ children }: PropsWithChildren) => {
     const isAuth = useAppSelector(selectIsAuthenticated)
+    const user = useAppSelector(selectUser)
     const location = useLocation()
+    const dispatch = useAppDispatch()
+    const hasSignedOut = useRef(false)
+
+    const allowed = isAllowedUser(user?.email ?? undefined)
+
+    useEffect(() => {
+        // If authenticated but not in the allowed list, sign them out
+        if (isAuth && !allowed && !hasSignedOut.current) {
+            hasSignedOut.current = true
+            dispatch(signOutThunk())
+        }
+    }, [isAuth, allowed, dispatch])
 
     if (!isAuth) {
         return <Navigate to="/login" state={{ from: location }} replace />
+    }
+
+    if (!allowed) {
+        return <Navigate to="/login" replace />
     }
 
     return children
